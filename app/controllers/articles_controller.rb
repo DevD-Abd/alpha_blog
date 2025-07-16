6f7e0242a3c25ef521cd3eb42b79228783e39377
DevD-Abd <abdullahubaidullah@carecloud.com>
2025-07-16 06:55:38 -0400

    create article from browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,19 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was successfully created."
+            redirect_to @article
+            # redirect_to articles_path(@article) #this will take to the index page/ where articles are listed
+
+        else
+            render 'new'  
+            # render 'articles/new' #this will take to the new page where we can create a new article
+        end
+    end
 end
