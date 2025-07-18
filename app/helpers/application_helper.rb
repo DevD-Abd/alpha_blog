05f183ed1a0cce44b434df28d9517abdc324c8aa
DevD-Abd <abdullahubaidullah@carecloud.com>
2025-07-18 01:52:53 -0400

    add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def gravatar_for(user, options = { size: 80 })
+      email_hash = Digest::MD5::hexdigest(user.email.downcase)
+      size = options[:size]
+      image_tag("https://www.gravatar.com/avatar/#{email_hash}?s=#{size}", alt: user.username, class: "gravatar rounded-circle", style: "width: #{size}px; height: #{size}px;")
+    end
 end
