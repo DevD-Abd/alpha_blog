0e1b8d618b0d5472b6ce786d30fbe7c4ea4313e1
DevD-Abd <abdullahubaidullah@carecloud.com>
2025-07-18 02:10:36 -0400

    index users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def index
+        @users = User.all
+    end
+
     def show
         @user = User.find(params[:id])
         @articles = @user.articles
@@ -16,7 +20,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your account was updated successfully"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
