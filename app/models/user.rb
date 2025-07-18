3d83426e01ff4fd169aa2ffa4710cbc527bb7346
DevD-Abd <abdullahubaidullah@carecloud.com>
2025-07-18 01:15:42 -0400

    add users create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
+    has_secure_password
     
     has_many :articles
     validates :username, presence: true, 
@@ -10,5 +11,4 @@ class User < ApplicationRecord
                                         uniqueness: { case_sensitive: false}, 
                                         length: { maximum: 105 }, 
                                         format: { with: VALID_EMAIL_REGEX }
-    has_secure_password
 end
